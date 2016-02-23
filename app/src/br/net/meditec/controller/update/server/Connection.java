6e7138295080b38689a77e3386dc1d2ea09be839
Adriano Pauli <adrianopauli03@gmail.com>
2016-02-22 23:19:20 -0300

    #2 -> Criacao do servidor fake link de atualizacao;

--- app/src/br/net/meditec/controller/update/server/Connection.java
+++ app/src/br/net/meditec/controller/update/server/Connection.java
@@ -9,8 +9,8 @@ public abstract class Connection {
     public static final String SERVER_TAG_LOGIN = "login";
     public static final String SERVER_TAG_PASSWORD = "senha";
     
-    public static final String SERVER_URL_XMl = "http://192.168.3.101:8080";
-    public static final String SERVER_MY_EVENT = SERVER_URL_XMl+"/autenticacao/autenticacao.php";
-    public static final String SERVER_EVENTOS = SERVER_URL_XMl+"/meditec/eventos.xml";
-    public static final String SERVER_PALESTRANTES = SERVER_URL_XMl+"/meditec/palestrantes.xml";
+    public static final String SERVER_URL_XMl = "https://raw.githubusercontent.com";
+    public static final String SERVER_MY_EVENT = SERVER_URL_XMl+"/adrianopauli/app_android_meditec/master/fake_server/eventos.xml";
+    public static final String SERVER_EVENTOS = SERVER_URL_XMl+"/adrianopauli/app_android_meditec/master/fake_server/eventos.xml";
+    public static final String SERVER_PALESTRANTES = SERVER_URL_XMl+"/adrianopauli/app_android_meditec/master/fake_server/palestrantes.xml";
 }
